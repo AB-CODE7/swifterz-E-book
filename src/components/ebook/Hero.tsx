87796517bbfa79d1ee054aeeed399853e905ca8e
Lakshmi Prasanna <prasannanagisetti415@gmail.com>
2026-05-14 16:38:26 +0530

    updated code

--- src/components/ebook/Hero.tsx
+++ src/components/ebook/Hero.tsx
@@ -81,14 +81,14 @@ const Hero: React.FC = () => {
             className="flex flex-col sm:flex-row items-center justify-between gap-8 py-6 mt-4 border-t border-gray-50"
           >
             <div className="flex items-center gap-4 flex-1">
-              <img src={CustomerServiceIcon} alt="Customer Service" className="w-12 h-12 shrink-0 object-contain" />
-              <p className="text-lg md:text-xl font-sora font-bold text-[#1F2937] leading-tight">
+              <img src={CustomerServiceIcon} alt="Customer Service" className="w-14 h-14 shrink-0 object-contain ml-6" />
+              <p className="text-lg md:text-lg font-sora font-bold text-[#1F2937] leading-tight ml-6">
                 Get a Professional Assessment of <br /> Your Project Risks
               </p>
             </div>
 
             <a href="https://swifterz.ae/book-a-consultation" target="_blank" rel="noopener noreferrer">
-              <button className="bg-[#926B15] text-white px-16 py-4 rounded-md font-sora font-bold  transition-all shadow-md hover:shadow-lg whitespace-nowrap">
+              <button className="bg-[#926B15] text-white px-10 py-3 rounded-md font-sora font-bold  transition-all shadow-md hover:shadow-lg whitespace-nowrap">
                 Consult For FREE
               </button>
             </a>
